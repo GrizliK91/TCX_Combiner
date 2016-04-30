881b276647972e988a10814e42a453a9b7ba7487
Alexander <grizlik91@gmail.com>
2016-04-30 13:05:37 +0300

    Added files via upload

---
+++ TCX Combiner/TCX Combiner/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace TCX_Combiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_load_endo = new System.Windows.Forms.Button();
+            this.button_load_garmin = new System.Windows.Forms.Button();
+            this.button_combine = new System.Windows.Forms.Button();
+            this.button_save = new System.Windows.Forms.Button();
+            this.listBox_endo = new System.Windows.Forms.ListBox();
+            this.listBox_garm = new System.Windows.Forms.ListBox();
+            this.listBox_output = new System.Windows.Forms.ListBox();
+            this.label_count_heart = new System.Windows.Forms.Label();
+            this.label_count_time = new System.Windows.Forms.Label();
+            this.button_clear_endo = new System.Windows.Forms.Button();
+            this.button_clear_garm = new System.Windows.Forms.Button();
+            this.button_clear_combine = new System.Windows.Forms.Button();
+            this.label_dragndrop1 = new System.Windows.Forms.Label();
+            this.label_dragndrop2 = new System.Windows.Forms.Label();
+            this.label_about = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_load_endo
+            // 
+            this.button_load_endo.Location = new System.Drawing.Point(12, 12);
+            this.button_load_endo.Name = "button_load_endo";
+            this.button_load_endo.Size = new System.Drawing.Size(140, 23);
+            this.button_load_endo.TabIndex = 1;
+            this.button_load_endo.Text = "Endomondo TCX Load";
+            this.button_load_endo.UseVisualStyleBackColor = true;
+            this.button_load_endo.Click += new System.EventHandler(this.button_load_endo_Click);
+            // 
+            // button_load_garmin
+            // 
+            this.button_load_garmin.Location = new System.Drawing.Point(347, 12);
+            this.button_load_garmin.Name = "button_load_garmin";
+            this.button_load_garmin.Size = new System.Drawing.Size(140, 23);
+            this.button_load_garmin.TabIndex = 3;
+            this.button_load_garmin.Text = "Garmin TCX Load";
+            this.button_load_garmin.UseVisualStyleBackColor = true;
+            this.button_load_garmin.Click += new System.EventHandler(this.button_load_garmin_Click);
+            // 
+            // button_combine
+            // 
+            this.button_combine.Enabled = false;
+            this.button_combine.Location = new System.Drawing.Point(685, 12);
+            this.button_combine.Name = "button_combine";
+            this.button_combine.Size = new System.Drawing.Size(140, 23);
+            this.button_combine.TabIndex = 8;
+            this.button_combine.Text = "Combine";
+            this.button_combine.UseVisualStyleBackColor = true;
+            this.button_combine.Click += new System.EventHandler(this.button_combine_Click);
+            // 
+            // button_save
+            // 
+            this.button_save.Enabled = false;
+            this.button_save.Location = new System.Drawing.Point(685, 493);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(318, 27);
+            this.button_save.TabIndex = 12;
+            this.button_save.Text = "Save file";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // listBox_endo
+            // 
+            this.listBox_endo.AllowDrop = true;
+            this.listBox_endo.FormattingEnabled = true;
+            this.listBox_endo.Location = new System.Drawing.Point(12, 41);
+            this.listBox_endo.Name = "listBox_endo";
+            this.listBox_endo.Size = new System.Drawing.Size(318, 446);
+            this.listBox_endo.TabIndex = 15;
+            this.listBox_endo.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox_endo_DragDrop);
+            this.listBox_endo.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox_endo_DragEnter);
+            // 
+            // listBox_garm
+            // 
+            this.listBox_garm.AllowDrop = true;
+            this.listBox_garm.FormattingEnabled = true;
+            this.listBox_garm.Location = new System.Drawing.Point(347, 41);
+            this.listBox_garm.Name = "listBox_garm";
+            this.listBox_garm.Size = new System.Drawing.Size(318, 446);
+            this.listBox_garm.TabIndex = 16;
+            this.listBox_garm.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox_garm_DragDrop);
+            this.listBox_garm.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox_garm_DragEnter);
+            // 
+            // listBox_output
+            // 
+            this.listBox_output.FormattingEnabled = true;
+            this.listBox_output.Location = new System.Drawing.Point(685, 41);
+            this.listBox_output.Name = "listBox_output";
+            this.listBox_output.Size = new System.Drawing.Size(318, 446);
+            this.listBox_output.TabIndex = 17;
+            // 
+            // label_count_heart
+            // 
+            this.label_count_heart.AutoSize = true;
+            this.label_count_heart.Location = new System.Drawing.Point(347, 493);
+            this.label_count_heart.Name = "label_count_heart";
+            this.label_count_heart.Size = new System.Drawing.Size(67, 13);
+            this.label_count_heart.TabIndex = 18;
+            this.label_count_heart.Text = "Count Heart:";
+            // 
+            // label_count_time
+            // 
+            this.label_count_time.AutoSize = true;
+            this.label_count_time.Location = new System.Drawing.Point(509, 493);
+            this.label_count_time.Name = "label_count_time";
+            this.label_count_time.Size = new System.Drawing.Size(64, 13);
+            this.label_count_time.TabIndex = 19;
+            this.label_count_time.Text = "Count Time:";
+            // 
+            // button_clear_endo
+            // 
+            this.button_clear_endo.Enabled = false;
+            this.button_clear_endo.Location = new System.Drawing.Point(190, 12);
+            this.button_clear_endo.Name = "button_clear_endo";
+            this.button_clear_endo.Size = new System.Drawing.Size(140, 23);
+            this.button_clear_endo.TabIndex = 20;
+            this.button_clear_endo.Text = "Clear";
+            this.button_clear_endo.UseVisualStyleBackColor = true;
+            this.button_clear_endo.Click += new System.EventHandler(this.button_clear_endo_Click);
+            // 
+            // button_clear_garm
+            // 
+            this.button_clear_garm.Enabled = false;
+            this.button_clear_garm.Location = new System.Drawing.Point(525, 12);
+            this.button_clear_garm.Name = "button_clear_garm";
+            this.button_clear_garm.Size = new System.Drawing.Size(140, 23);
+            this.button_clear_garm.TabIndex = 21;
+            this.button_clear_garm.Text = "Clear";
+            this.button_clear_garm.UseVisualStyleBackColor = true;
+            this.button_clear_garm.Click += new System.EventHandler(this.button_clear_garm_Click);
+            // 
+            // button_clear_combine
+            // 
+            this.button_clear_combine.Enabled = false;
+            this.button_clear_combine.Location = new System.Drawing.Point(863, 12);
+            this.button_clear_combine.Name = "button_clear_combine";
+            this.button_clear_combine.Size = new System.Drawing.Size(140, 23);
+            this.button_clear_combine.TabIndex = 22;
+            this.button_clear_combine.Text = "Clear";
+            this.button_clear_combine.UseVisualStyleBackColor = true;
+            this.button_clear_combine.Click += new System.EventHandler(this.button_clear_combine_Click);
+            // 
+            // label_dragndrop1
+            // 
+            this.label_dragndrop1.AutoSize = true;
+            this.label_dragndrop1.BackColor = System.Drawing.SystemColors.Window;
+            this.label_dragndrop1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_dragndrop1.Location = new System.Drawing.Point(46, 240);
+            this.label_dragndrop1.Name = "label_dragndrop1";
+            this.label_dragndrop1.Size = new System.Drawing.Size(226, 24);
+            this.label_dragndrop1.TabIndex = 23;
+            this.label_dragndrop1.Text = "Drop here to load your file";
+            // 
+            // label_dragndrop2
+            // 
+            this.label_dragndrop2.AutoSize = true;
+            this.label_dragndrop2.BackColor = System.Drawing.SystemColors.Window;
+            this.label_dragndrop2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_dragndrop2.Location = new System.Drawing.Point(387, 240);
+            this.label_dragndrop2.Name = "label_dragndrop2";
+            this.label_dragndrop2.Size = new System.Drawing.Size(226, 24);
+            this.label_dragndrop2.TabIndex = 24;
+            this.label_dragndrop2.Text = "Drop here to load your file";
+            // 
+            // label_about
+            // 
+            this.label_about.AutoSize = true;
+            this.label_about.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_about.ForeColor = System.Drawing.Color.Blue;
+            this.label_about.Location = new System.Drawing.Point(9, 505);
+            this.label_about.Name = "label_about";
+            this.label_about.Size = new System.Drawing.Size(38, 15);
+            this.label_about.TabIndex = 25;
+            this.label_about.Text = "About";
+            this.label_about.Click += new System.EventHandler(this.label_about_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 532);
+            this.Controls.Add(this.label_about);
+            this.Controls.Add(this.label_dragndrop2);
+            this.Controls.Add(this.label_dragndrop1);
+            this.Controls.Add(this.button_clear_combine);
+            this.Controls.Add(this.button_clear_garm);
+            this.Controls.Add(this.button_clear_endo);
+            this.Controls.Add(this.label_count_time);
+            this.Controls.Add(this.label_count_heart);
+            this.Controls.Add(this.listBox_output);
+            this.Controls.Add(this.listBox_garm);
+            this.Controls.Add(this.listBox_endo);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.button_combine);
+            this.Controls.Add(this.button_load_garmin);
+            this.Controls.Add(this.button_load_endo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TCX Combiner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_load_endo;
+        private System.Windows.Forms.Button button_load_garmin;
+        private System.Windows.Forms.Button button_combine;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.ListBox listBox_endo;
+        private System.Windows.Forms.ListBox listBox_garm;
+        private System.Windows.Forms.ListBox listBox_output;
+        private System.Windows.Forms.Label label_count_heart;
+        private System.Windows.Forms.Label label_count_time;
+        private System.Windows.Forms.Button button_clear_endo;
+        private System.Windows.Forms.Button button_clear_garm;
+        private System.Windows.Forms.Button button_clear_combine;
+        private System.Windows.Forms.Label label_dragndrop1;
+        private System.Windows.Forms.Label label_dragndrop2;
+        private System.Windows.Forms.Label label_about;
+    }
+}
+
---
+++ TCX Combiner/TCX Combiner/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TCX_Combiner
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string str;
+        string time_string = "";
+
+        string fileName_garm;
+        string fileName_endo;
+
+        TimeSpan time_endo;
+        TimeSpan time_gar;
+        TimeSpan time_rez;
+        TimeSpan time_value = new TimeSpan(0, 0, 13);
+
+        int count_time_gar_string = 0;
+        int count_heart_string = 0;
+
+        bool find_time = false;
+        bool flag_timecount = false;
+        bool flag_heartcount = false;
+        bool flag_containsFlag = false;
+
+        List<string> list_heart_value = new List<string>();
+        List<string> list_time_value = new List<string>();
+
+        private void load_garmin_file()
+        {
+            listBox_garm.Items.Clear();
+            string line_garm;
+            using (var reader = File.OpenText(fileName_garm))
+            {
+                while ((line_garm = reader.ReadLine()) != null)
+                {
+                    listBox_garm.Items.Add(line_garm.Replace(" ", ""));
+                }
+            }
+            list_heart_value.Clear();
+            list_time_value.Clear();
+            foreach (var item in listBox_garm.Items)
+            {
+                str = item.ToString();
+                if (time_string != "")
+                {
+                    if (str.Contains("<Value>") == true)
+                    {
+                        list_heart_value.Add("<HeartRateBpm>" + str + "</HeartRateBpm>");
+                        list_time_value.Add(time_string.Substring(17, 8));
+                        time_string = "";
+                    }
+                }
+                if (str.Contains("<Time>") == true)
+                    time_string = str;
+            }
+
+            if (list_heart_value.Count > 0 && list_time_value.Count > 0)
+            {
+                label_count_heart.Text = "Count Heart: " + list_heart_value.Count.ToString();
+                label_count_time.Text = "Count Time: " + list_time_value.Count.ToString();
+
+                button_load_garmin.Enabled = false;
+                button_clear_garm.Enabled = true;
+                listBox_garm.AllowDrop = false;
+                label_dragndrop2.Visible = false;
+
+                if (listBox_endo.Items.Count > 0)
+                    button_combine.Enabled = true;
+            }
+            else
+            {
+                list_heart_value.Clear();
+                list_time_value.Clear();
+                listBox_garm.Items.Clear();
+                MessageBox.Show("We don't find any heart beat date!");
+            }
+        }
+
+        private void load_endo_file()
+        {
+            string line_endo;
+            using (var reader = File.OpenText(fileName_endo))
+            {
+                while ((line_endo = reader.ReadLine()) != null)
+                {
+                    if (flag_containsFlag == false)
+                    {
+                        listBox_endo.Items.Add(line_endo);
+                        if (line_endo.Contains("<Track>") == true)
+                            flag_containsFlag = true;
+                    }
+                    else
+                    {
+                        if (line_endo.Contains("Activity Sport") == true || line_endo.Contains("Lap StartTime") == true)
+                            listBox_endo.Items.Add(line_endo);
+                        else
+                            listBox_endo.Items.Add(line_endo.Replace(" ", ""));
+                    }
+                }
+                flag_containsFlag = false;
+
+                button_load_endo.Enabled = false;
+                button_clear_endo.Enabled = true;
+                listBox_endo.AllowDrop = false;
+                label_dragndrop1.Visible = false;
+
+                if (listBox_garm.Items.Count > 0)
+                    button_combine.Enabled = true;
+            }
+        }
+
+        private void button_load_endo_Click(object sender, EventArgs e)
+        {
+            Stream mystream_endo;
+            OpenFileDialog commandFileDialog_endo = new OpenFileDialog();
+            commandFileDialog_endo.Filter = "TCX files (*.tcx)|*.tcx";
+            commandFileDialog_endo.InitialDirectory = @"C:\";
+            commandFileDialog_endo.Title = "Please select Endomondo TCX file";
+            if (commandFileDialog_endo.ShowDialog() == DialogResult.OK)
+            {
+                listBox_endo.Items.Clear();
+                if ((mystream_endo = commandFileDialog_endo.OpenFile()) != null)
+                {
+                    fileName_endo = commandFileDialog_endo.FileName;
+                    load_endo_file();
+                }
+            }
+        }
+
+        private void button_load_garmin_Click(object sender, EventArgs e)
+        {
+            Stream mystream_garm;
+            OpenFileDialog commandFileDialog_garm = new OpenFileDialog();
+            commandFileDialog_garm.Filter = "TCX files (*.tcx)|*.tcx";
+            commandFileDialog_garm.InitialDirectory = @"C:\";
+            commandFileDialog_garm.Title = "Please select Garmin TCX file";
+            if (commandFileDialog_garm.ShowDialog() == DialogResult.OK)
+            {
+                if ((mystream_garm = commandFileDialog_garm.OpenFile()) != null)
+                {
+                    if (commandFileDialog_garm.FileName.Contains("tcx") == false)
+                        MessageBox.Show("Upload only TCX file!");
+                    else
+                    {
+                        fileName_garm = commandFileDialog_garm.FileName;
+                        load_garmin_file();
+                    }
+                }
+            }
+        }
+
+        private void button_combine_Click(object sender, EventArgs e)
+        {
+            listBox_output.Items.Clear();
+            time_gar = TimeSpan.Parse(list_time_value[count_time_gar_string].ToString());
+            foreach (var items in listBox_endo.Items)
+            {
+                str = items.ToString();
+
+                if (str.Contains("<Time>") == true && flag_timecount == false)
+                {
+                    time_endo = TimeSpan.Parse(str.Substring(17, 8));
+                    time_rez = time_gar - time_endo;
+                    if (time_rez < time_value)
+                    {
+                        find_time = true;
+
+                        if (list_time_value.Count > count_time_gar_string)
+                        {
+                            time_gar = TimeSpan.Parse(list_time_value[count_time_gar_string].ToString());
+                            count_time_gar_string++;
+                        }
+                        else
+                            flag_timecount = false;
+                    }
+                }
+                if (find_time == true && flag_heartcount == false && flag_timecount == false)
+                {
+                    find_time = false;
+                    listBox_output.Items.Add(str);
+                    if (list_heart_value.Count > count_heart_string)
+                    {
+                        listBox_output.Items.Add(list_heart_value[count_heart_string]);
+                        count_heart_string++;
+                    }
+                    else
+                        flag_heartcount = false;
+                }
+                else
+                    listBox_output.Items.Add(str);
+            }
+            button_clear_combine.Enabled = true;
+            button_save.Enabled = true;
+        }
+
+        
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            saveFile.Filter = "TCX File (*.tcx)|*.tcx";
+            saveFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFile.FileName = "TCX_Combiner_output";
+            if (saveFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                using (var sw = new StreamWriter(saveFile.FileName, false))
+                    foreach (var item in listBox_output.Items)
+                        sw.Write(item.ToString() + Environment.NewLine);
+                MessageBox.Show("Success");
+            }
+        }
+
+        private void listBox_endo_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files_endo = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files_endo[0].Contains(".tcx") == false)
+                MessageBox.Show("Upload only TCX file!");
+            else
+            {
+                listBox_endo.Items.Clear();
+                fileName_endo = files_endo[0];
+                load_endo_file();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox_endo_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox_garm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files_garm = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files_garm[0].Contains(".tcx") == false)
+                MessageBox.Show("Upload only TCX file!");
+            else
+            {
+                fileName_garm = files_garm[0];
+                load_garmin_file();
+            }
+        }
+
+        private void listBox_garm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void button_clear_garm_Click(object sender, EventArgs e)
+        {
+            listBox_garm.Items.Clear();
+            button_clear_garm.Enabled = false;
+            listBox_garm.AllowDrop = true;
+            button_load_garmin.Enabled = true;
+            button_combine.Enabled = false;
+            label_count_heart.Text = "Count Heart: ";
+            label_count_time.Text = "Count Time: ";
+            label_dragndrop2.Visible = true;
+        }
+
+        private void button_clear_endo_Click(object sender, EventArgs e)
+        {
+            listBox_endo.Items.Clear();
+            button_clear_endo.Enabled = false;
+            listBox_endo.AllowDrop = true;
+            button_load_endo.Enabled = true;
+            button_combine.Enabled = false;
+            label_dragndrop1.Visible = true;
+        }
+
+        private void button_clear_combine_Click(object sender, EventArgs e)
+        {
+            listBox_output.Items.Clear();
+            button_clear_combine.Enabled = false;
+            button_save.Enabled = false;
+        }
+
+        private void label_about_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("TCX_Combiner\nApplication combine TCX from Endomondo App and Garmin Vivofit2\nAlexander Ivanov 2016","About",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+    }
+}
